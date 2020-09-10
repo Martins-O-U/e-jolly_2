78a17abb40e5286accd8c82de134a23b1f5e6055
HP <martinsonyedikachi@gmail.com>
2020-09-10 10:10:06 +0100

    Data Tables created

---
+++ database/migrations/20200910073027_jolly_table.js
@@ -0,0 +1,62 @@
+
+exports.up = function (knex) {
+    return knex.schema
+        .createTable('EventPlanners', table => {
+            table.increments('id');
+            table.integer('phoneNumber', 20);
+            table.varchar('username').notNullable();
+            table.string('email', 128).unique().notNullable();
+            table.varchar('location');
+            table.timestamps('created_at');
+        })
+        .createTable('Events', table => {
+            table.increments('id');
+            table.varchar('eventName').notNullable();
+            table.integer('event_id', 5).unsigned().notNullable()
+                .references('id').inTable('EventPlanners').onDelete('CASCADE').onUpdate('CASCADE');
+        })
+        .createTable('Tables', table => {
+            table.increments('id');
+            table.integer("table", 5);
+        })
+        .createTable('FoodList', table => {
+            table.increments('id');
+            table.string("foodName", 128);
+        })
+        .createTable('DrinkList', table => {
+            table.increments('id');
+            table.string("foodName", 128);
+        })
+        .createTable('GuestList', table => {
+            table.increments('id');
+            table.string('guestName').notNullable();
+            table.integer('totalInvitedPersons');
+            table.integer('totalNumberOfTables');
+            table.integer('guestEvent_id', 5).unsigned().notNullable()
+                .references('id').inTable('Events').onDelete('CASCADE').onUpdate('CASCADE');
+            table.integer('guestTable_id', 5).unsigned().notNullable()
+                .references('id').inTable('Tables').onDelete('CASCADE').onUpdate('CASCADE');
+            table.integer('guestFood', 5).unsigned().notNullable()
+                .references('id').inTable('FoodList').onDelete('CASCADE').onUpdate('CASCADE');
+            table.integer('guestDrink', 5).unsigned().notNullable()
+                .references('id').inTable('DrinkList').onDelete('CASCADE').onUpdate('CASCADE');
+        })
+        .createTable('NumberPerGuest', table => {
+            table.increments('id');
+            table.integer("invitedPersons");
+            table.integer('numberPerGuest_id', 5).unsigned().notNullable()
+                .references('id').inTable('GuestList').onDelete('CASCADE').onUpdate('CASCADE');
+        })
+
+};
+
+exports.down = function (knex) {
+    return knex.schema
+        .dropTableIfExists('EventPlanners')
+        .dropTableIfExists('Events')
+        .dropTableIfExists('GuestList')
+        .dropTableIfExists('NumberPerGuest')
+        .dropTableIfExists('Tables')
+        .dropTableIfExists('FoodList')
+        .dropTableIfExists('DrinkList');
+};
